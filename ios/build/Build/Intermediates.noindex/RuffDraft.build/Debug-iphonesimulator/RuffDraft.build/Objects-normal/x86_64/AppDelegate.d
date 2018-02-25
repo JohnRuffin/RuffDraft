e3f0c40fcee2bee07fd799e66ea46fea29fad301
John Ruffin <john@johnruffin.io>
2018-02-24 19:09:33 -0500

    after adding app center specifics

--- ios/build/Build/Intermediates.noindex/RuffDraft.build/Debug-iphonesimulator/RuffDraft.build/Objects-normal/x86_64/AppDelegate.d
+++ ios/build/Build/Intermediates.noindex/RuffDraft.build/Debug-iphonesimulator/RuffDraft.build/Objects-normal/x86_64/AppDelegate.d
@@ -1,12 +1,16 @@
 dependencies: \
   /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/RuffDraft/AppDelegate.m \
   /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/RuffDraft/AppDelegate.h \
-  /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBundleURLProvider.h \
-  /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTRootView.h \
+  /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/AppCenterReactNativeCrashes/AppCenterReactNativeCrashes.h \
+  /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTEventEmitter.h \
   /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridge.h \
   /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeDelegate.h \
   /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTJavaScriptLoader.h \
   /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
   /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
   /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTFrameUpdate.h \
-  /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h
+  /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h \
+  /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/AppCenterReactNativeAnalytics/AppCenterReactNativeAnalytics.h \
+  /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/AppCenterReactNative/AppCenterReactNative.h \
+  /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBundleURLProvider.h \
+  /Users/kingofcode/myDev/reactNativeProjects/RuffDraft/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTRootView.h
